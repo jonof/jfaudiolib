77a9ce3c4e7e339c2780a7bbe7b667b2019b7c3b
Jonathon Fowler <jf@jonof.id.au>
2018-11-21 22:27:02 +1000

    more dicking around building osx xiph libraries

--- third-party/osx/build.sh
+++ third-party/osx/build.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-oggurl=http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
-vorbisurl=http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
+oggbase=https://downloads.xiph.org/releases/ogg
+oggfile=libogg-1.3.2.tar.gz
+oggfilesum=e19ee34711d7af328cb26287f4137e70630e7261b17cbe3cd41011d73a654692
+vorbisbase=https://downloads.xiph.org/releases/vorbis
+vorbisfile=libvorbis-1.3.5.tar.gz
+vorbisfilesum=6efbcecdd3e5dfbf090341b485da9d176eb250d893e3eb378c428a2db38301ce
+
+oggurl=$oggbase/$oggfile
+vorbisurl=$vorbisbase/$vorbisfile
+
+# Inherit build architectures from the environment given by Xcode.
+arches=()
+test -n "$ARCHS" && arches=($ARCHS)
+archflags="${arches[*]/#/-arch }"
+
+destdir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 export MAKE="xcrun make"
 export CC="xcrun clang"
@@ -10,7 +24,7 @@ export LD="xcrun ld"
 export AR="xcrun ar"
 export RANLIB="xcrun ranlib"
 export STRIP="xcrun strip"
-export CFLAGS="-arch i386 -arch x86_64 -mmacosx-version-min=10.8"
+export CFLAGS="$archflags -mmacosx-version-min=10.8"
 
 check_tools() {
     echo "+++ Checking build tools"
@@ -20,48 +34,66 @@ check_tools() {
     fi
 }
 
-if test ! -f libogg.a; then
+check_file() {
+    local shasum=$1
+    local filename=$2
+    shasum -a 256 -c -s - <<EOT
+$shasum  $filename
+EOT
+}
+
+if test ! -f $destdir/out/lib/libogg.a; then
     check_tools
 
     mkdir libogg-build
 
     echo "+++ Fetching and unpacking $oggurl"
-    curl -s $oggurl | (cd libogg-build; tar zx --strip-components 1) || exit
+    if [ -f $oggfile ]; then
+        check_file $oggfilesum $oggfile || rm -f $oggfile
+    fi
+    if [ ! -f $oggfile ]; then
+        curl -sL $oggurl -o $oggfile || exit
+        check_file $oggfilesum $oggfile || rm -f $oggfile
+    fi
+    (cd libogg-build; tar zx --strip-components 1) < $oggfile || exit
 
     echo "+++ Configuring libogg"
-    (cd libogg-build; ./configure) || exit
+    (cd libogg-build; ./configure --prefix=/out) || exit
 
     echo "+++ Building libogg"
     (cd libogg-build; $MAKE) || exit
 
-    echo "+++ Collecting libogg build products"
-    mkdir ogg
-    cp libogg-build/src/.libs/libogg.a .
-    cp libogg-build/include/ogg/*.h ogg
+    echo "+++ Installing libogg to $destdir"
+    (cd libogg-build; $MAKE DESTDIR=$destdir install) || exit
 
-    echo "+++ Cleaning up libogg"
-    rm -rf libogg-build
+    # echo "+++ Cleaning up libogg"
+    # rm -rf libogg-build
 fi
 
-if test ! -f libvorbisfile.a; then
+if test ! -f $destdir/out/lib/libvorbisfile.a; then
     check_tools
 
     mkdir libvorbis-build
 
     echo "+++ Fetching and unpacking $vorbisurl"
-    curl -s $vorbisurl | (cd libvorbis-build; tar zx --strip-components 1) || exit
+    if [ -f $vorbisfile ]; then
+        check_file $vorbisfilesum $vorbisfile || rm -f $vorbisfile
+    fi
+    if [ ! -f $vorbisfile ]; then
+        curl -sL $oggurl -o $vorbisfile || exit
+        check_file $vorbisfilesum $vorbisfile || rm -f $vorbisfile
+    fi
+    (cd libvorbis-build; tar zx --strip-components 1) < $vorbisfile || exit
 
     echo "+++ Configuring libvorbis"
-    (cd libvorbis-build; ./configure --with-ogg-libraries=$(pwd)/.. --with-ogg-includes=$(pwd)/..) || exit
+    (cd libvorbis-build; PKG_CONFIG=/usr/bin/false ./configure --prefix=/out --with-ogg=$destdir/out) || exit
 
     echo "+++ Building libvorbis"
     (cd libvorbis-build; $MAKE) || exit
 
-    echo "+++ Collecting libvorbis build products"
-    mkdir vorbis
-    cp libvorbis-build/lib/.libs/libvorbis{,file}.a .
-    cp libvorbis-build/include/vorbis/{codec,vorbisfile}.h vorbis
+    echo "+++ Installing libvorbis to $destdir"
+    (cd libvorbis-build; $MAKE DESTDIR=$destdir install) || exit
 
-    echo "+++ Cleaning up libvorbis"
-    rm -rf libvorbis-build
+    # echo "+++ Cleaning up libvorbis"
+    # rm -rf libvorbis-build
 fi
