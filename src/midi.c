064ee434ffd914815bb688fc36d20ceb2cb3f277
Jonathon Fowler <jf@jonof.id.au>
2025-08-18 21:38:04 +1000

    remove unused ll_man.h

--- src/midi.c
+++ src/midi.c
@@ -39,7 +39,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 #include "sndcards.h"
 #include "drivers.h"
-#include "ll_man.h"
 #include "music.h"
 #include "_midi.h"
 #include "midi.h"
--- src/music.c
+++ src/music.c
@@ -34,7 +34,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "drivers.h"
 #include "music.h"
 #include "midi.h"
-#include "ll_man.h"
 #include "asssys.h"
 #include "assmisc.h"
 
